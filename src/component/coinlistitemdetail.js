7f9b307f57f677ebdda96ebbbc653e301076d27d
zorro3255 <zorro3255@naver.com>
2022-04-26 23:59:20 +0900

    removing not used Link Element

--- src/component/coinlistitemdetail.js
+++ src/component/coinlistitemdetail.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
 import News from './news';
 import Header from './header';
 import {Navigation} from './navigation';
