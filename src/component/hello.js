0eda9a2fa6aa12665bc00edeb95b5453e55139e5
zorro3255 <zorro3255@naver.com>
2022-05-02 23:31:57 +0900

    이미지 세로 가운데 정렬 수정

--- src/component/hello.js
+++ src/component/hello.js
@@ -6,7 +6,7 @@ class Hello extends React.Component {
     render(){
         return (
             <React.Fragment>
-                <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', minHeight:'100vh'}}>
+                <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', minHeight:'95vh'}}>
                     <img width="30%" src={bitcoin} alt="bitcoin" />
                     <div><Link to="/coinlist">들어가기</Link></div>
                 </div>
