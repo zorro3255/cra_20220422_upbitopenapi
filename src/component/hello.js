369b93d4d8ff4179205f34e6413a1fba0cdaec40
zorro3255 <zorro3255@naver.com>
2022-05-03 23:18:25 +0900

    메인 이미지 문구 수정

--- src/component/hello.js
+++ src/component/hello.js
@@ -5,7 +5,8 @@ import {SettingConsumer} from '../context/context';
 
 class Hello extends React.Component {
     state={
-        seconds: 10
+        seconds: 10,
+        text: '앱을 초기화 하는 중입니다.'
     }
 
     componentDidMount(){
@@ -16,7 +17,12 @@ class Hello extends React.Component {
             clearInterval(timer);this.props.navigate('/coinlist')
         }})}, 1000);
         */
-       setTimeout(()=>{this.props.navigate('/coinlist')}, 3000);
+
+        const a = setInterval(()=>{
+            this.setState({text: `${this.state.text}.`})
+        }, 1000);
+
+       setTimeout(()=>{clearInterval(a); this.props.navigate('/coinlist')}, 7000);
     }
 
     render() {
@@ -26,7 +32,7 @@ class Hello extends React.Component {
                     <img width="30%" src={bitcoin} alt="bitcoin" />
                     {/*<div><Link to="/coinlist">들어가기</Link></div>
                     <div>{this.state.seconds}초후 목록으로 이동</div>*/}
-                    <div>앱을 초기화 하는 중입니다...</div>
+                    <div>{this.state.text}</div>
                     {/*
                         <SettingConsumer>{
                             value => <div style={{margin:'10px 0px'}}><input type="radio" id="ko" name="languageSetting" value="ko" checked={value.state.lang==='ko'?true:false} onChange={() => value.action.setLang('ko')} /><label htmlFor="ko">한국어</label>&nbsp;&nbsp;&nbsp;<input type="radio" id="en" name="languageSetting" value="en" onChange={() => value.action.setLang('en')} checked={value.state.lang==='en'?true:false} /><label htmlFor="en">영어</label></div>}               
